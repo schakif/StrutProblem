57a547174e8be9aada755f10e8d66ebbfb0c1a4d
Steve Chakif <schakif@Steves-MacBook-Pro-2.local>
2014-02-10 16:50:02 -0500

    Initial Commit

---
+++ HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Steve Chakif on 2/10/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by Steve Chakif on 2/10/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
